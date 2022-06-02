691497be52be6f71d24ddf314572d675ddd16577
rTaiShan <raul.tai@usp.br>
2022-06-02 11:32:41 -0300

    Added multi-language support

--- bs.py
+++ bs.py
@@ -1,11 +1,22 @@
+import locale
 from random import choice
 
-def generateBs():
+def generateBs(locale):
     returnString = ""
-    parts = [["Found nothing, ", "Empty, "], ["just ", "except for "], ["words", "more words"]]
+    locales = {
+        "en" : [["Found nothing, ", "Empty, "], ["just ", "except for "], ["words", "more words"]]
+    }
+    if locale not in locales:
+        return ""
+    parts = locales[locale]
     for part in parts:
         returnString += choice(part)
     return returnString
 
 if __name__ == "__main__":
-    print(generateBs())
+    locale = input("Insert locale: ")
+    generatedString = generateBs(locale)
+    while (not generatedString):
+        locale = input("Invalid locale, try again: ")
+        generatedString = generateBs(locale) 
+    print(generatedString)
