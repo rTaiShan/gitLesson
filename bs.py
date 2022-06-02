15acec16365447c0d7e0f54d3e6632780dbabd5e
rTaiShan <raul.tai@usp.br>
2022-06-02 11:23:15 -0300

    Refactored bs.py for scalability

--- bs.py
+++ bs.py
@@ -1,10 +1,11 @@
 from random import choice
 
 def generateBs():
-    part1 = ["Found nothing, ", "Empty, "]
-    part2 = ["just ", "except for "]
-    part3 = ["words", "more words"]
-    return (choice(part1) + choice(part2) + choice(part3))
+    returnString = ""
+    parts = [["Found nothing, ", "Empty, "], ["just ", "except for "], ["words", "more words"]]
+    for part in parts:
+        returnString += choice(part)
+    return returnString
 
 if __name__ == "__main__":
     print(generateBs())
